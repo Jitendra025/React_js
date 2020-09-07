36617509e5625907b09fb89499d14bc57d450925
Jitendra025 <jmg0625@gmail.com>
2020-09-07 22:57:31 +0100

    Burger Buildr Module 8 finished

--- The Burger Builder-Section8/burgerbuilder/src/components/UI/Navigation/Toolbar/Toolbar.js
+++ The Burger Builder-Section8/burgerbuilder/src/components/UI/Navigation/Toolbar/Toolbar.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import classes from './Toolbar.module.css';
 import Logo from '../../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
-
-const toolbar=(props)=>(
-
+import classes from './Toolbar.module.css';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
+const toolbar = ( props ) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo/>
-        <nav>
-           <NavigationItems/>
-
+        <DrawerToggle clicked={props.drawerToggleClicked} />
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems />
         </nav>
-
     </header>
 );
 export default toolbar;
